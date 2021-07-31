9e17afae3a5216f9b73646cb1d87421e37fc071a
Laura Kapitza <laura.kapitza@gmail.com>
2021-07-31 09:47:42 +0200

    add code to signup component

--- olympi-client/src/App.js
+++ olympi-client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.scss';
+import './App.css';
 
 import {Switch, Route} from 'react-router-dom';
 
--- olympi-client/src/components/auth/Signup.js
+++ olympi-client/src/components/auth/Signup.js
@@ -6,7 +6,7 @@ import authService from './auth-service.js';
 
 export default class extends React.Component {
   state = {
-    professional: false,
+    professional: 0,
     username: "",
     email: "",
     password: "",
@@ -17,7 +17,16 @@ export default class extends React.Component {
     website: "",
     about: "",
 
+    open: false,
     error: ""
+  }
+
+  toggle = () => {
+    this.setState({open: !this.state.open})
+  }
+
+  changeProfessional = (props) => {
+    this.setState({professional: props})
   }
 
   handleSubmit = (event) => {
@@ -69,77 +78,94 @@ export default class extends React.Component {
     return (
         <>
           <h1>Sign up</h1>
+
+          <h2>Choose your profile</h2>
+
+          {/* this.state.professional=false */}
+          <button onClick={() => {this.setState({professional:false})}}>I am a normal user</button> 
+
+          {/* this.state.professional=true */}
+          <button professional={true} onClick={() => {this.setState({professional:true})}}>I am a professional</button>
           
-          <form onSubmit={this.handleSubmit}>
+          {this.state.professional !== 0 && (
+            
+            <form onSubmit={this.handleSubmit}>
 
-            {this.state.error && (
-              <p className="error">{this.state.error}</p>
+              {/* for an error */}
+              {this.state.error && (
+                <p className="error">{this.state.error}</p>
+              )}
+
+              <p>
+                <label>
+                  <em>Username</em>
+                  <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+                </label>
+              </p>
+
+              <p>
+                <label>
+                  <em>Email</em>
+                  <input type="email" name="email" value={this.state.email} onChange={this.handleChange} />
+                </label>
+              </p>
+
+              <p>
+                <label>
+                  <em>Password</em>
+                  <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                </label>
+              </p>
+
+              <p>
+                <label>
+                  <em>City</em>
+                  <input type="text" name="city" value={this.state.city} onChange={this.handleChange} />
+                </label>
+              </p>
+
+              <p>
+                <label>
+                  <em>Favorite Exercise</em>
+                  <input type="text" name="fav_exercise" value={this.state.fav_exercise} onChange={this.handleChange} />
+                </label>
+              </p>
+              
+              {/* for professional user */}
+              {this.state.professional === true && (
+                <>
+                  <p>
+                    <label>
+                      <em>Career Start</em>
+                      <input type="month" name="career_date" value={this.state.career_date} onChange={this.handleChange} />
+                    </label>
+                  </p>
+
+                  <p>
+                    <label>
+                      <em>Certifications</em>
+                      <input type="text" name="certifications" value={this.state.certifications} onChange={this.handleChange} />
+                    </label>
+                  </p>
+
+                  <p>
+                    <label>
+                      <em>Website (optional)</em>
+                      <input type="text" name="website" value={this.state.website} onChange={this.handleChange} />
+                    </label>
+                  </p>
+
+                  <p>
+                    <label>
+                      <em>About you</em>
+                      <input type="text" name="about" value={this.state.about} onChange={this.handleChange} />
+                    </label>
+                  </p>
+                </>
             )}
+            </form>
 
-            <p>
-              <label>
-                <em>Username</em>
-                <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
-              </label>
-            </p>
-
-            <p>
-              <label>
-                <em>Email</em>
-                <input type="email" name="email" value={this.state.email} onChange={this.handleChange} />
-              </label>
-            </p>
-
-            <p>
-              <label>
-                <em>Password</em>
-                <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
-              </label>
-            </p>
-
-            <p>
-              <label>
-                <em>City</em>
-                <input type="text" name="city" value={this.state.city} onChange={this.handleChange} />
-              </label>
-            </p>
-
-            <p>
-              <label>
-                <em>Favorite Exercise</em>
-                <input type="text" name="fav_exercise" value={this.state.fav_exercise} onChange={this.handleChange} />
-              </label>
-            </p>
-
-            <p>
-              <label>
-                <em>Career Start</em>
-                <input type="month" name="career_date" value={this.state.career_date} onChange={this.handleChange} />
-              </label>
-            </p>
-
-            <p>
-              <label>
-                <em>Certifications</em>
-                <input type="text" name="certifications" value={this.state.certifications} onChange={this.handleChange} />
-              </label>
-            </p>
-
-            <p>
-              <label>
-                <em>Website (optional)</em>
-                <input type="text" name="website" value={this.state.website} onChange={this.handleChange} />
-              </label>
-            </p>
-
-            <p>
-              <label>
-                <em>About you</em>
-                <input type="text" name="about" value={this.state.about} onChange={this.handleChange} />
-              </label>
-            </p>
-
-          </form>
+          )}
 
           <p>
             <small>If you already have an account, you can login from <Link to="/login">here</Link></small>
