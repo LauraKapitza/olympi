6d395d9c5ecd0e953050adb825c12e47fae07091
potauro <karinagonzalezsanchez@gmail.com>
2021-08-03 00:03:55 +0200

    footer

--- olympi-client/src/components/home/Footer/Footer.js
+++ olympi-client/src/components/home/Footer/Footer.js
@@ -1,13 +1,49 @@
 import React from 'react'
 import "./Footer.css";
+import {Link} from 'react-router-dom';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
 
 
 function Footer() {
   return (
     <div className= "FooterContainer">
+      <div className="FooterLeft">
+      <img src="/assets/logos/olympifigure.png" alt="" className="logo"/><img src="/assets/logos/olympiword.png" alt="" className="logo"/>  </div>
+        
 
-      this is the footer
+      <div className="FooterCenterLeft"> 
+      <h3>Company</h3>
+      <Link to="/about" >About</Link>
+      <Link to="/team" >Team</Link>
+      <Link to="/faq" >FAQ</Link>
+      <Link to="/terms" >Terms & Conditions</Link>
+      </div>
+ 
+      <div className="FooterCenterRight"> 
+      <h3>Resources</h3>
+      <Link to="/professionals" >For Professionals</Link>
+      <Link to="/definitions" >Tag Definitions</Link>
+      <Link to="/blog" >Blog</Link>
+      </div>
+
+
+      <div className="FooterRight">
+      <h3>Contact Us</h3>
+      <span>+33 07 66 33 78 48 </span>
+      <span>olympi.app@gmail.com</span>
+      </div>
+
+      <div className="Instagram">
+      <Link to="https://www.instagram.com/olympi_app/" ><InstagramIcon/></Link>
+
+      </div>
+
     </div>
+
+
+
+
   )
 }
 
