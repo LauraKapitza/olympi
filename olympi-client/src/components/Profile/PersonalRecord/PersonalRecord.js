09128b0f4d21cb17ca7c671bff21a9a4f958091a
potauro <karinagonzalezsanchez@gmail.com>
2021-08-03 23:01:24 +0200

    part 2 of user profile css

--- olympi-client/src/components/Profile/PersonalRecord/PersonalRecord.js
+++ olympi-client/src/components/Profile/PersonalRecord/PersonalRecord.js
@@ -5,14 +5,61 @@ function PersonalRecord() {
   return (
     <div className="PersonalRecordContainer">
       
-      Personal Record
+      <div className="PRtitle">Personal Record
+
+      <div className="EditPr">Edit<img src="/assets/icons/editbutton.svg"></img>
+
+      </div></div>
 
       <div className="RecordTableContainer">
-        wekflw
+               <table>
+                <thead>
+                <tr>
+                    <th>Exercise</th>
+                    <th>Weight</th>
+                    <th>Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> Squat</td>
+                        <td> 50 kg</td>
+                        <td> 10 May 2021</td>
+                        
+                    </tr>
+
+                    <tr>
+                        <td> Deadlift </td>
+                        <td> 50 kg</td>
+                        <td> 10 May 2021</td>
+                        
+                    </tr>
+
+                    
+                   
+                    <tr>
+                        <td> Bench Press </td>
+                        <td> 50 kg</td>
+                        <td> 10 May 2021</td>
+                        </tr>
+                    
+
+                    <tr>
+                        <td> Overhead Press </td>
+                        <td> 50 kg</td>
+                        <td> 10 May 2021</td>
+                        
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        
+
       </div>
 
 
-    </div>
+    
   )
 }
 
