526450958238cf1aacdadffb839e1f7f5fec0da4
potauro <karinagonzalezsanchez@gmail.com>
2021-08-04 20:57:09 +0200

    explore page css done

--- olympi-client/src/components/Explore/ExploreVideos/ExploreVideos.js
+++ olympi-client/src/components/Explore/ExploreVideos/ExploreVideos.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import MoreIcon from '@material-ui/icons/More';
-import "./ExploreVideos.css"
+import React from "react";
+import MoreIcon from "@material-ui/icons/More";
+import "./ExploreVideos.css";
 
 function ExploreVideos() {
   return (
     <div className="ExploreVideosContainer">
-      This section will hold the video feed for explore
-      <MoreIcon/>
-    </div>
-  )
+      <div className="VideoSpace">
+        <div className="VideoTitle">username 10 May 2021</div>
+        <img src="https://via.placeholder.com/250"></img>
+        <div className="VidOptions">
+          <div>
+            <img src="/assets/icons/upvotefull.svg"></img>2.3k
+          </div>
+          <MoreIcon />
+        </div>
+
+        <div className="VideoTitle">username 10 May 2021</div>
+        <img src="https://via.placeholder.com/250"></img>
+        <div className="VidOptions">
+          <div>
+            <img src="/assets/icons/upvotefull.svg"></img>2.3k
+          </div>
+          <MoreIcon />
+        </div>
+
+          <div className="VideoTitle">username 10 May 2021</div>
+          <img src="https://via.placeholder.com/250"></img>
+          <div className="VidOptions">
+            <div>
+              <img src="/assets/icons/upvotefull.svg"></img>2.3k
+            </div>
+            <MoreIcon />
+          </div>
+        </div>
+      </div>
+    
+    
+  );
 }
 
-export default ExploreVideos
+export default ExploreVideos;
